b64f245c18b0e417aed3b9bc6ea542ad652609d0
Fred Lopez <fred@fredlopez.com>
2021-04-19 12:22:17 -0700

    2nd commit

---
+++ ReduxCounter/src/Components/Increments.js
@@ -0,0 +1,29 @@
+import {connect} from 'react-redux'
+
+function Increments (props) {
+
+const up = () => {
+    props.onIncrement()
+}
+
+const down = () => {
+    props.onDec()
+}
+
+    return (
+        <div>
+            <h3>Increments Component</h3>
+            <button onClick={up}>+</button> <button onClick={down}>-</button>
+        </div>
+    )
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        onIncrement: () => dispatch({type: 'INCREMENT'}),
+        onDec: () => dispatch({type: 'DECRIMENT'})
+    }
+}
+
+
+export default connect(null, mapDispatchToProps)(Increments)
---
+++ ReduxCounter/src/store/reducer.js
@@ -0,0 +1,61 @@
+
+const initialState = {
+    ctr: 0,
+    name: 'John Doe'
+}
+
+const reducer = (state = initialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            ...state,
+            ctr: state.ctr + 1
+        }
+    }
+    if (action.type === 'DECRIMENT') {
+        return {
+            ...state,
+            ctr: state.ctr - 1
+        }
+    }
+    if (action.type === 'ADD') {
+        console.log(typeof(action.value))
+        return {
+            ...state,
+            ctr: state.ctr + action.value
+        }
+    }
+    if (action.type === "SUB") {
+        return {
+            ...state,
+            ctr: state.ctr - action.value
+        }
+    }
+    if (action.type === 'MATH') {
+        if(action.method === '+') {
+            return {
+                ...state,
+                ctr: state.ctr + action.value
+        }}
+        if(action.method === '-') {
+            return {
+                ...state,
+                ctr: state.ctr - action.value
+        }}
+        if(action.method === '*') {
+            return {
+                ...state,
+                ctr: state.ctr * action.value
+        }}
+        if(action.method === '/') {
+            return {
+                ...state,
+                ctr: state.ctr / action.value
+        }
+        }
+    }
+    
+    
+    return state
+}
+
+export default reducer
